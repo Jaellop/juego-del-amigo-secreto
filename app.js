f056b59f1d822f086f541da96c32e49fcc23dbe6
Alaine Amador <jaellop@gmail.com>
2025-02-07 22:43:05 -0600

    Agregar funcion de actualizar lista de amigos

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let listaAmigosAgregados = [];
-//Establecer variable que almacena en lista los nombres de las personas que particpan en el concurso
-console.log(nombreDeAmigosSecretos);
+//Establecer variable que almacena en lista los nombres de las personas que particpan en el concurs
 
 //Desarrolla una función, que permita al usuario ingresar un nombre en el campo de texto y añadirlo a la lista de amigos creada anteriormente.
  function agregarAmigo () {
  let nombreDeAmigoSecreto = document.getElementById("amigo").value.trim();
+ console.log(nombreDeAmigoSecreto);
+
     if (nombreDeAmigoSecreto === "") {
         alert ("Por favor, inserte un nombre");
     } else {
@@ -14,13 +15,27 @@ console.log(nombreDeAmigosSecretos);
             return;
         } else {
             listaAmigosAgregados.push(nombreDeAmigoSecreto);
-            return nombreDeAmigoSecreto;
+            actualizarListaDeAmigos ();
+            limpiarCaja();   
         }
-      
+
     }
-    limpiarCaja();   
     
  }
+
+function actualizarListaDeAmigos () {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    for (let i = 0; i < listaAmigosAgregados.length; i++) { 
+        let li = document.createElement("li"); // Crea un nuevo elemento <li>
+        li.textContent = listaAmigosAgregados[i]; // Asigna el nombre del amigo al <li>
+        lista.appendChild(li); // Agrega el <li> a la lista <ul>
+    }
+    
+
+}
+
 
 
  function limpiarCaja(){
